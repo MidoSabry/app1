a294403ea605ef5237e5ddbb2d280f50db1b29a4
Mido <ms259915@gmail.com>
2021-03-23 10:28:59 +0200

    first app

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('First App',textAlign: TextAlign.center,),
+          backgroundColor: Colors.green,
+          centerTitle: true,
+
+
+        ),
+        body: Center(
+
+          child: Column(
+
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Hello World',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+
+
+
